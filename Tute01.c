fb1d86cd0c97c09547c998a5581fdcf2790c9dd4
Rickz137 <119772400+Rickz137@users.noreply.github.com>
2023-03-12 12:44:06 +0000

    Finished Tute 2,3 and 4.

--- Tute01.c
+++ Tute01.c
@@ -9,10 +9,11 @@ int main() {
   float mark1, mark2, avg;
 
   printf("Enter Marks[1]:");
-  scanf("%d", &mark1);
+  scanf("%f", &mark1);
 
   printf("Enter Marks[2]:");
   scanf("%f", &mark2);
+  
   avg = (mark1 + mark2) / 2.0;
 
   printf("Average : %.2f", avg);
